d8fb5b7b4219467608b1f33912b77a456e64260f
AlberVicky09 <al.rodriguezg.2018@alumnos.urjc.es>
2022-07-26 17:02:43 +0200

    Version 0.1
    First functional version

---
+++ TeethVR_V2/Assets/PaperPlaneTools/ARMarker/Scripts/ProjectTeethCleaningScript.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProjectTeethCleaningScript : MonoBehaviour
+{
+    public Text actCounter;
+
+    public ProjectController gameController;
+    private Renderer rend;
+    private Texture2D tex;
+    private Texture2D originalTex;
+    private Vector2 pixelUV;
+    private Color[] colors;
+
+    private Vector3 originPos;
+    private bool cleaned = false;
+    private int numCleaned = 0;
+    private const int MAXCLEANING = 5;
+    
+    public void StartClean(){
+
+        rend = GetComponent<Renderer>();
+
+        originalTex = rend.material.mainTexture as Texture2D;
+
+        tex = new Texture2D(originalTex.width, originalTex.height);
+        tex.SetPixels(originalTex.GetPixels());
+        rend.material.mainTexture = tex;
+        tex.Apply(true);
+
+        tex.SetPixels32(gameController.whiteArray);
+        tex.Apply(true);
+        rend.material.mainTexture = tex;
+        
+        GetComponent<MeshCollider>().enabled = false;
+    }
+
+    public void StartGreen(){
+
+        rend = GetComponent<Renderer>();
+
+        originalTex = rend.material.mainTexture as Texture2D;
+
+        tex = new Texture2D(originalTex.width, originalTex.height);
+        tex.SetPixels(originalTex.GetPixels());
+        rend.material.mainTexture = tex;
+        tex.Apply(true);
+
+        tex.SetPixels32(gameController.greenArray);
+        tex.Apply(true);
+        rend.material.mainTexture = tex;
+        
+        GetComponent<MeshCollider>().enabled = false;
+    }
+
+    public void TurnClean(){
+    
+        tex.SetPixels32(gameController.whiteArray);
+        tex.Apply(true);
+        rend.material.mainTexture = tex;
+    }
+
+    public void TurnGreen(){
+
+        tex.SetPixels32(gameController.greenArray);
+        tex.Apply(true);
+        rend.material.mainTexture = tex;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(!cleaned){
+            originPos = other.transform.position;
+            //actCounter.gameObject.transform.parent.gameObject.SetActive(true);
+            //actCounter.text = this.name + "\n" + numCleaned + "/" + MAXCLEANING;
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if(!cleaned){
+            if(Vector3.Distance(other.transform.position, originPos) > 0.05f){
+
+                numCleaned++;
+                //Update counter
+                //actCounter.text = this.name + "\n" + numCleaned + "/" + MAXCLEANING;
+
+                if(numCleaned != MAXCLEANING){
+                    //Change color
+                    switch(numCleaned){
+                        case 1: tex.SetPixels32(gameController.firstColorArray); break;
+                        case 2: tex.SetPixels32(gameController.secondColorArray); break;
+                        case 3: tex.SetPixels32(gameController.thirdColorArray); break;
+                        case 4: tex.SetPixels32(gameController.fourthColorArray); break;
+                    }
+                    tex.Apply(true);
+                    rend.material.mainTexture = tex;
+                }else{
+                    //Change color
+                    tex.SetPixels32(gameController.whiteArray);
+                    tex.Apply(true);
+                    rend.material.mainTexture = tex;
+                    //End cleaning for this tooth
+                    cleaned = true;
+                    //gameController.AddCleaned();
+                }
+            }
+        }
+    }
+
+    /*private void OnCollisionEnter(Collision other) {
+
+            RaycastHit hit = new RaycastHit();
+            Ray ray = new Ray(other.GetContact(0).point - other.GetContact(0).normal, other.GetContact(0).normal);
+            if(Physics.Raycast(ray, out hit)){
+                Debug.Log(hit.textureCoord);
+                pixelUV = hit.textureCoord;
+                pixelUV.x *= tex.width;
+                pixelUV.y *= tex.height;
+
+                tex.SetPixels((int) pixelUV.x, (int) pixelUV.y, 100, 100, colors);
+                tex.Apply(true);
+                
+                rend.material.mainTexture = tex;
+            }
+            Debug.Log(this.name);
+    }*/
+}
